cb42167bed642468b9a5abc2572188690edd5cf2
daniel.obara <daniel.tsutomu@hotmail.com>
2019-01-25 16:22:31 -0200

    Criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">This is a Home Page</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Link to="/about" className="App-link">
+              Let's go to About Page ;D
+            </Link>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import About from "./components/About";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Registrations from "./components/Registrations";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// BrowserRouter teremos as rotas para os componentes a partir desta chamada. Deste modo ele ficara em volta de todos os outros componentes.
+// Switch funciona como se fosse a condicional 'switch/case'. Determina qual rota será redirecionada conforme a selecionada.
+// Route é a rota que receberá qual o caminho e qual o componente que deve ser chamado.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// O path="/" pode ser usado junto com o exact para garantir que se a rota tiver somente "/" ele vai ser renderizado.
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/" exact={true} component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/registrations" component={Registrations} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
