be1bf5e5912f541587ba238189fcd4dedb3b81a6
Daniel Obara <DanielObara@users.noreply.github.com>
2019-01-26 18:08:56 -0200

    Alteração do nome do componente para Profile e remoção de icone .svg.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import logo from "../react-router-logo.svg";
+import logo from "../react-router.png";
 import "../App.css";
 
-/* Importe o Link do React-Router para poder usá-lo para redirecionar ao link especificado passado por parametro*/
+/* Importe o Link do React-Router para poder usá-lo 
+para redirecionar ao link especificado passado por parametro*/
 import { Link } from "react-router-dom";
 
 export default class About extends Component {
@@ -13,15 +14,16 @@ export default class About extends Component {
           <h1 className="App-title">This is a About Page</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            {/* Aqui é o mesmo conceito sobre como utilizar o Link do react-router-dom. Apenas informe para onde ir */}
+            {/* Aqui é o mesmo conceito sobre como utilizar o Link do react-router-dom.
+             Apenas informe para onde ir */}
             <Link to="/" className="App-link">
               Let's go back to the home page
             </Link>
           </p>
           <h3>Or</h3>
           <p>
-            <Link to="/registrations" className="App-link">
-              Let's go back to the registrations page
+            <Link to="/profile" className="App-link">
+              Let's go back to the Profile page
             </Link>
           </p>
         </header>
