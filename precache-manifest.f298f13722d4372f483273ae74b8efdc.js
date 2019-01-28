2cf048ec70d48e961862f0a419117468620f3432
daniel.obara <daniel.tsutomu@hotmail.com>
2019-01-28 11:03:29 -0200

    Updates

---
+++ precache-manifest.f298f13722d4372f483273ae74b8efdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3dc54dabeadca048f20",
+    "url": "%20//danielobara.github.io/My-react-router-app/static/js/runtime~main.d3dc54da.js"
+  },
+  {
+    "revision": "36997fb270c2309026b7",
+    "url": "%20//danielobara.github.io/My-react-router-app/static/js/main.36997fb2.chunk.js"
+  },
+  {
+    "revision": "98d41acaa43c476fc368",
+    "url": "%20//danielobara.github.io/My-react-router-app/static/js/1.98d41aca.chunk.js"
+  },
+  {
+    "revision": "36997fb270c2309026b7",
+    "url": "%20//danielobara.github.io/My-react-router-app/static/css/main.acbfa782.chunk.css"
+  },
+  {
+    "revision": "9ca6615585c37f600dcaf446afcf8f29",
+    "url": "%20//danielobara.github.io/My-react-router-app/index.html"
+  }
+];
